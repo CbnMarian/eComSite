3a4b22fac3e6738276ea24a1fb3b54f4bcb4d905
CbnMarian <m.marian03@yahoo.com>
2023-11-14 15:42:51 +0200

    added home and started shop page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-
 import HomePage from "./pages/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">SHOP PAGE</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/collection-preview/collection-preview.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./collection-preview.scss";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{Title.toUpperCase()}</h1>
+    <div className="preview">
+      {items.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
