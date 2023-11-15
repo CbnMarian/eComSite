7c4e6d05d603cd53b935d66df9707ec8145ccf02
CbnMarian <m.marian03@yahoo.com>
2023-11-15 10:21:15 +0200

    added sign in page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignOutPage from "./pages/sign-inand-sing-up/sign-in-signup";
 
 function App() {
   return (
@@ -15,11 +16,15 @@ function App() {
           <li>
             <Link to="/shop">SHOP PAGE</Link>
           </li>
+          <li>
+            <Link to="/signin">signin</Link>
+          </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/signin" element={<SignInAndSignOutPage />} />
       </Routes>
     </>
   );
