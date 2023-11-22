2a99c4bed4b16de190043a67c4b988af97556ab6
CbnMarian <m.marian03@yahoo.com>
2023-11-22 11:32:23 +0200

    adeed add to cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -13,7 +13,7 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -25,21 +25,25 @@ const Header = ({ currentUser }) => (
       <Link className="option" to="/shop">
         SHOP
       </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
       {currentUser ? (
         <div className="option" onClick={() => auth.signOut()}>
-          Sign Out
+          SIGN OUT
         </div>
       ) : (
         <Link className="option" to="/signin">
-          Sign In
+          SIGN IN
         </Link>
       )}
       <CartIcon />
     </div>
-    <CartDropDown />
+    {hidden ? null : <CartDropDown />}
   </div>
 );
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 export default connect(mapStateToProps)(Header);
